d61ea38dc9d6b4301c715387aae5f00c6af0a4c1
mohamedbashandy <mohamed.bashandy.bemo@gmail.com>
2026-08-26 07:40:07 +0300

    use tailwind in our project

---
+++ projects/react-press/src/UI/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="min-h-screen bg-red-500 p-10">
+            <h1 className="text-5xl font-bold text-white">React Press</h1>
+        </div>
+    )
+}
