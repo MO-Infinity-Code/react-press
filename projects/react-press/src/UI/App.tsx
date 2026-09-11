1c42dc883b4f9a886320c4a961b2a9a133fb7457
mohamedbashandy <mohamed.bashandy.bemo@gmail.com>
2026-09-11 16:59:13 +0300

    Add Commet To App TSX

--- projects/react-press/src/UI/App.tsx
+++ projects/react-press/src/UI/App.tsx
@@ -4,6 +4,7 @@ const App = () => {
     return (
         <div className="content">
             <h1>Rsbuild with React</h1>
+            {/* qdwedc */}
             <p>Start building amazing things with Rsbuild.</p>
         </div>
     )
